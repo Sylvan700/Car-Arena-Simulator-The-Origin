a11ecf6387612f345cb51c62b2a428185463ff41
Sylvan <sylvan700@hotmail.fr>
2019-05-21 12:45:18 +0200

    Build 0.1

---
+++ Assets/Scripts/VehiculeTemplate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehiculeTemplate : MonoBehaviour
+{
+
+    // Weird "instanciation/declaration" of our rigidbody  
+    public Rigidbody2D bodyCar;
+    // Variable vertical speed of the car
+    public float speed = 200.0f;
+    public float maxSpeed;
+    // Variable rotation speed of the car
+    public float rotationForce = 20.0F;
+    public float steering;
+    // var for the lifes of the car
+    public int lifePoint = 3;
+
+}
