2cd14551b6c1813ca33f60eb12d87afff8442864
Sylvan <sylvan700@hotmail.fr>
2019-05-27 17:00:11 +0200

    Build 7

---
+++ Assets/Scripts/ScoreText2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreText2 : MonoBehaviour
+{
+
+    public Text labelScore;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        labelScore.text = "Player Three " + Manager.scorePlayer3.ToString() + " - " + Manager.scorePlayer4.ToString() + " Player Four";
+
+    }
+}
